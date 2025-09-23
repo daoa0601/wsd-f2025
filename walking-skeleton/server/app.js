ab7bd97f8e79cfeac266862af99c000b0e0b4787
Anh Dao <anhdd2000@gmail.com>
2025-09-23 20:02:40 +0300

    full-stack skeleton

--- walking-skeleton/server/app.js
+++ walking-skeleton/server/app.js
@@ -1,8 +1,12 @@
 import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
+// importing database client
+import postgres from "postgres";
 
 const app = new Hono();
+// creating an instance of the database client
+const sql = postgres();
 
 app.use("/*", cors());
 app.use("/*", logger());
@@ -11,6 +15,12 @@ let visits = 0;
 app.get("/api/visits", (c) => {
   visits++;
   return c.json({ visits });
+});
+
+// retrieving todos from database on requests to /api/todos
+app.get("/api/todos", async (c) => {
+  const todos = await sql`SELECT * FROM todos`;
+  return c.json(todos);
 });
 
 export default app;
